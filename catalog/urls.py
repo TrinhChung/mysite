8e431367990f6097c083ba54edc7298cde56ea18
TrinhChung <chungtrinh2k2@gmail.com>
2023-07-04 11:57:12 +0700

    Working with forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,13 +5,19 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("i18n/", include("django.conf.urls.i18n")),
     path("books/", views.BookListView.as_view(), name="books"),
-    path("authors/", views.AuthorListView.as_view(), name="authors"),
-    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
-    path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
-    path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
     path(
         "bookinst-manage/",
         views.LoanedBooksManageListView.as_view(),
         name="bookinst-manage",
     ),
+    path(
+        "book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"
+    ),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
+    path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
+    path("authors/", views.AuthorListView.as_view(), name="authors"),
+    path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
+    path("author/create/", views.AuthorCreate.as_view(), name="author-create"),
+    path("author/<int:pk>/update/", views.AuthorUpdate.as_view(), name="author-update"),
+    path("author/<int:pk>/delete/", views.AuthorDelete.as_view(), name="author-delete"),
 ]
